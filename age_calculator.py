c9ea72a79d944c5b2de0aa03e293c927336e386a
iajackson <34674321+iajackson@users.noreply.github.com>
2018-05-14 21:24:23 +1000

    Update age_calculator.py

--- age_calculator.py
+++ age_calculator.py
@@ -18,11 +18,16 @@ def calculate_age():
 
 root = Tk()
 root.title("Age calculator")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
-frame = ttk.Frame(root, width = 640, height = 480, relief='raised', padding = "20")
+frame = ttk.Frame(root, relief='raised', padding = "10")
 frame.grid(column=0, row=0, sticky=(N, S, E, W))
 frame.columnconfigure(0, weight=1)
+frame.columnconfigure(1, weight=1)
 frame.rowconfigure(0, weight=1)
+frame.rowconfigure(1, weight=1)
+frame.rowconfigure(2, weight=1)
 
 label = ttk.Label(frame, text = "What is your age in years?")
 label.grid(column=0, row=0)
@@ -32,13 +37,13 @@ text_entry = ttk.Entry(frame, width=10, textvariable=age)
 text_entry.grid(column=1, row=0)
 
 go_button = ttk.Button(frame, text = 'Calculate!', command = calculate_age)
-go_button.grid(column=0, row=2)
+go_button.grid(column=0, row=1)
 
 age_in_days = StringVar()
-label2 = ttk.Label(frame, text = "Your are this many days old:")
-label2.grid(column=0, row=3)
+label2 = ttk.Label(frame, text = "You are this many days old:")
+label2.grid(column=0, row=2)
 
 label3 = ttk.Label(frame, textvariable = age_in_days)
-label3.grid(column=1, row=3)
+label3.grid(column=1, row=2)
 
 root.mainloop()
